622a013d99d4a73e7fd5b118f2522bdf5e755e6f
Javier Almarza <javieralmarzabucarey@gmail.com>
2020-04-21 21:20:09 -0400

    organizacion de paquetes

---
+++ src/Ferrantis/Init.java
@@ -0,0 +1,13 @@
+package Ferrantis;
+
+// Importación de paquetes.
+import Areas.*;
+import Menu.*;
+import Vehiculo.*;
+
+public class Init {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+}
--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package laboratorio;
+package Menu;
 
 /**
  *
  * @author javie
  */
-public class Laboratorio {
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class MainMenu {
     
 }
