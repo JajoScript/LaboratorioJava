bbc5984e6943cf454c0017e18db0e7e05b280334
Javier Almarza <javieralmarzabucarey@gmail.com>
2020-04-24 16:01:19 -0400

    implementando todo

---
+++ src/areasDeTrabajo/Administracion.java
@@ -0,0 +1,15 @@
+package areasDeTrabajo;
+
+// Importación de clases.
+
+
+// Creación de la clase administración.
+public abstract class Administracion {
+    // atributos.
+    
+    // Constructor.
+    
+    // Metodos.
+    
+    // Getters & Setters.
+}
---
+++ src/empleados/Administrador.java
@@ -0,0 +1,11 @@
+package empleados;
+
+// Importación de clases.
+import areasDeTrabajo.*;
+
+public class Administrador extends Administracion{
+    // Atributos.
+    public String nombre = "Javier Almarza";
+    
+    // Metodos.
+}
